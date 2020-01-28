cb02767e597226e404a9a18aa3350f1ec5c07a27
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-01-28 21:50:08 +0000

    added register functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from accounts.forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 def index(request):
     """
@@ -8,6 +9,7 @@ def index(request):
     """
     return render(request, 'index.html')
 
+@login_required
 def logout(request):
     """
     Log the user out
@@ -20,22 +22,49 @@ def login(request):
     """
     Return the login page
     """
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
-                                    password=request.POST['password'])
-            
-
+                                     password=request.POST['password'])
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+def registration(request):
+    """
+    Render the registration form page
+    """
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password1'] )
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered!")
+                return redirect(reverse('index'))
+            else: 
+                messages.error(request, "Unable to register your account at thi time")
+
+    else:
+        registration_form = UserRegistrationForm()
+        
+    return render(request, 'registration.html', {"registration_form": registration_form})
 
 
 
