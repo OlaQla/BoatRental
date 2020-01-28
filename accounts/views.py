ba623726cc2f8994b3bf7c12320ca4a7ff78511b
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-01-28 14:15:59 +0000

    added html file, view, url and form for login

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
 
 def index(request):
     """
     Return the index.html 
     """
     return render(request, 'index.html')
+
+def logout(request):
+    """
+    Log the user out
+    """
+    auth.logout(request)
+    messages.success(request, "You have successfully been logged out")
+    return redirect(reverse('index'))
+
+def login(request):
+    """
+    Return the login page
+    """
+    login_form = UserLoginForm()
+    return render(request, 'login.html', {"login_form": login_form})
+
+
 
