6737b2491621aff1f391271ba9e72827050d5197
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-02-02 22:18:33 +0000

    url, views, html template for boat details

--- boats/urls.py
+++ boats/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import all_boats
+from .views import all_boats, boat_details
 
 urlpatterns = [
     url(r'^$', all_boats, name='boats'),
+    url(r'^(?P<boat_id>[0-9]+)$', boat_details, name="boat_details"),
 ]
--- boats/views.py
+++ boats/views.py
@@ -5,3 +5,9 @@ from .models import Boats
 def all_boats(request):
     boats = Boats.objects.all()
     return render(request, "boats.html", {"boats": boats})
+
+def boat_details(request, boat_id):
+    boat = Boats.objects.get(id=boat_id)
+    print(boat)
+    return render(request, "boat_details.html", {"boat": boat})
+
