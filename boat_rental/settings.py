8dc3e439fbcb3503a76466206d0971c7581810d5
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-02-04 20:59:24 +0000

    removed import env

--- boat_rental/settings.py
+++ boat_rental/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 import sys
-import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -52,6 +51,7 @@ INSTALLED_APPS = [
     'accounts',
     'boats',
     'cart',
+   
     
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
