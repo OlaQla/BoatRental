a3908cc83037619b8b131cd5feeb5050aee614f8
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-02-04 20:30:25 +0000

    setting up stripe and styles to forms login/signup

--- boat_rental/settings.py
+++ boat_rental/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 import sys
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -160,13 +161,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-                
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"            
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 AUTHENTICATION_BACKENDS = ('accounts.backends.EmailAuth', "django.contrib.auth.backends.ModelBackend")
 if 'test' not in sys.argv:
