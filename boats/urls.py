32dd045ad6b210fd5fe50ed6c8b7c94c43064071
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-02-11 21:25:42 +0000

    create boat availability endpoint

--- boats/urls.py
+++ boats/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_boats, boat_details
+from .views import all_boats, boat_details, boat_availability
 
 urlpatterns = [
     url(r'^$', all_boats, name='boats'),
     url(r'^(?P<boat_id>[0-9]+)$', boat_details, name="boat_details"),
+    url(r'^availability/(?P<boat_id>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)$', boat_availability, name="boat_availability")
 ]
--- boats/views.py
+++ boats/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Boats
+from checkout.models import OrderLineItem
+from django.http import HttpResponse
+import calendar
+from django.core.serializers import serialize
+import json
+from datetime import date, datetime, timedelta
+from collections import namedtuple
 
 def all_boats(request):
     boats = Boats.objects.all()
@@ -10,4 +17,25 @@ def boat_details(request, boat_id):
     boat = Boats.objects.get(id=boat_id)
     print(boat)
     return render(request, "boat_details.html", {"boat": boat})
+
+def boat_availability(request, boat_id, year, month):
+    daysTaken = {}
+    request_from_date = datetime(int(year), int(month), 1)
+    request_to_date = datetime(int(year), int(month), calendar.monthrange(int(year),int(month))[1])
+    orderDates = OrderLineItem.objects.filter(boat_id=boat_id).exclude(from_date__gte=request_to_date.timestamp()).exclude(to_date__lte=request_from_date.timestamp())
+    
+    for order in orderDates: 
+        startDate = date.fromtimestamp(order.from_date)
+        endDate = date.fromtimestamp(order.to_date)
+        while startDate <= endDate:
+            if startDate.month == int(month): 
+                daysTaken[startDate.day] = True 
+            startDate += timedelta(days=1)      
+
+    cal = calendar.Calendar()
+    dayarray = []
+    for day in cal.itermonthdates(int(year), int(month)):
+	    dayarray.append(day)
+    Availability = namedtuple('Availability', 'day in_month available')
+    return HttpResponse(json.dumps(list(map(lambda d: (Availability(day=d.day, in_month=d.month == int(month), available=d.day not in daysTaken))._asdict(), dayarray))), content_type='application/json')
 
