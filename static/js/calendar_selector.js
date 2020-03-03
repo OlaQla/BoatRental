5b962ab097cd287a145a4eff8a2f94adbd802c2e
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-03-03 21:09:35 +0000

    Loading calendar data for different months

--- static/js/calendar_selector.js
+++ static/js/calendar_selector.js
@@ -5,38 +5,55 @@ const dateForField = (date) => {
     return calYear + "-" + (calMonth) + "-" + (calDay)
 }
 
+const reloadCalendar = (boat_id, year, month) => 
+    $.ajax({url: `/boats/availability/${boat_id}/${year}/${month}`,})
+        .done(function (data) {
+            $("#calendar").find("td").each(function (index) {
+                $(this).text(data[index].day)
+
+                if (data[index].in_month) {
+                    $(this).addClass("in_month");
+                } else {
+                    $(this).removeClass("in_month");
+                }
+
+                if (data[index].available) {
+                    $(this).addClass("available");
+                } else {
+                    $(this).removeClass("available");
+                }
+
+                $(".month").text(new Date(year, month).toLocaleString('default', { month: 'long' }));
+
+            })
+    });
+
 $(document).ready(function () {
     date = new Date()
     boat_id = $(window.location.href.split("/")).last()[0];
-    $.ajax({
-        url: `/boats/availability/${boat_id}/${date.getFullYear()}/${date.getMonth()}`,
-
-    }).done(function (data) {
-        $("#calendar").find("td").each(function (index) {
-            $(this).text(data[index].day)
-
-            if (data[index].in_month) {
-                $(this).addClass("in_month")
-            }
-            if (data[index].available) {
-                $(this).addClass("available")
-            }
-
-        })
-
-    });
+    reloadCalendar(boat_id, date.getFullYear(), date.getMonth())
 
     let isFirst = true;
     let newStartDate = new Date();
     let newEndDate = new Date();
 
+    $buttons = $("[type=button]");
+
+    $buttons.eq(1).click(function(){
+        date = new Date(date.getFullYear(), date.getMonth() - 1);
+        reloadCalendar(boat_id, date.getFullYear(), date.getMonth() + 1);
+    });
+
+    $buttons.eq(2).click(function(){
+        date = new Date(date.getFullYear(), date.getMonth() + 1);
+        reloadCalendar(boat_id, date.getFullYear(), date.getMonth() + 1);
+    });
+
     $(document).on('click', '.in_month.available', function () {
 
-        currDate = new Date()
-
         calDay = $(this).text()
-        calMonth = currDate.getMonth();
-        calYear = currDate.getFullYear()
+        calMonth = date.getMonth();
+        calYear = date.getFullYear()
 
         if (isFirst) {
             newStartDate = new Date(calYear, calMonth, calDay)
