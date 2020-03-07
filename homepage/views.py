799350395d2882a838eb24985296d57d54e20fca
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-03-07 20:51:12 +0000

    Loading reviews from database

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from reviews.models import Reviews
+from collections import namedtuple
 
-def index(request):
-    """
-    Return the index.html
-    """
+ReviewView = namedtuple('ReviewView', ['reviewText', 'image', 'stars', 'stars_missing'])
+
+def index(request):  
+    # Get first three reviews
+    dbReviews = Reviews.objects.all()[:3]
+    reviews = map(lambda r: ReviewView(reviewText = r.reviewText, image = r.image, stars = range(r.starRating), stars_missing = range(5 - r.starRating)), dbReviews)
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'reviews': reviews})
