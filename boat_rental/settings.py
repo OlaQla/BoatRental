c483d17252d4f7055f341f0fb6ef4fe7af67a08f
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-03-19 21:21:55 +0000

    Serving static files from s3 bucket

--- boat_rental/settings.py
+++ boat_rental/settings.py
@@ -32,12 +32,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-AWS_STORAGE_BUCKET_NAME = 'boat.rental'
-AWS_S3_REGION_NAME = 'eu-west-1'
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -149,14 +143,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000', 
+}
+AWS_STORAGE_BUCKET_NAME = 'boat-rental'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+AWS_DEFAULT_ACL = 'public-read'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-STATICFILES_LOCATION = 'static'
-MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -177,4 +185,4 @@ AUTHENTICATION_BACKENDS = (
     'accounts.backends.EmailAuth',
     "django.contrib.auth.backends.ModelBackend")
 if 'test' not in sys.argv:
-    django_heroku.settings(locals())
+    django_heroku.settings(locals(), staticfiles=False)
