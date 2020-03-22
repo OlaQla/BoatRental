bcbdfb2c4c28a10e4612ee9d67dbd20df4969a9d
Aleksandra Kula <aleksandra.kula@outlook.com>
2020-03-22 20:25:22 +0000

    added coments to boat view and fixed responsive

--- boat_rental/settings.py
+++ boat_rental/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '033+%2bvaqz_a2t-m-c3sw1e!v^9t*g4*3xzzf@t8brt)2tp=('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
